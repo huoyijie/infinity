1f45d06aca6fd9399e72d43494c246a13575d2ab
huoyijie <yijie.huo@foxmail.com>
2023-08-03 21:51:09 +0800

    toolbar pencil size setting

--- frontend/components/PencilSetting.jsx
+++ frontend/components/PencilSetting.jsx
@@ -7,6 +7,7 @@ import WBContext from './WBContext';
 export default function () {
   const WB = useContext(WBContext);
   const [showPencilSetting, setShowPencilSetting] = useState(false);
+  const [pencilSize, setPencilSize] = useState(2);
 
   return (
     <div className="flex flex-row gap-1">
@@ -14,8 +15,13 @@ export default function () {
       {showPencilSetting && (
         <>
           <div className="rounded h-10 p-2">
-            <input id="default-range" type="range" defaultValue={2} min={1} max={20} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" onChange={(e) => WB.setSize(e.target.value)} />
+            <input id="default-range" type="range" defaultValue={2} min={1} max={20} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" onChange={(e) => {
+              const { value } = e.target;
+              WB.setSize(value);
+              setPencilSize(value);
+            }} />
           </div>
+          <div className="text-center p-2">{ pencilSize }</div>
           <div className="bg-slate-700 hover:bg-slate-300 hover:cursor-pointer active:bg-slate-500 w-10 h-10 rounded p-2 text-white" onClick={() => setShowPencilSetting(false)}>
             <Cancel />
           </div>
