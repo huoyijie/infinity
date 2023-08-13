47145ebc9c6abaf477fabe7489a46e01d7251fc3
huoyijie <yijie.huo@foxmail.com>
2023-08-13 12:42:05 +0800

    fix loading center of window

--- frontend/components/Spinner.jsx
+++ frontend/components/Spinner.jsx
@@ -2,7 +2,7 @@ import Spinner from "./assets/Spinner";
 
 export default function () {
   return (
-    <div className="h-full w-full fixed text-center p-72 bg-slate-300 opacity-75 z-[100]">
+    <div className="h-full w-full fixed flex justify-center items-center text-center bg-slate-300 opacity-75 z-[100]">
       <div role="status">
         <Spinner />
         <span className="sr-only">Loading...</span>
