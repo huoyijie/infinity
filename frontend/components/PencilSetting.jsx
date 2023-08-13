9d353b672a308b0073eb25634bc3d7e4b989aca5
huoyijie <yijie.huo@foxmail.com>
2023-08-13 11:04:18 +0800

    fix settings hide

--- frontend/components/PencilSetting.jsx
+++ frontend/components/PencilSetting.jsx
@@ -16,7 +16,7 @@ export default function ({ showPencilSetting }) {
         setShowPencilSetting(!showPencilSetting);
       }} />
       {showPencilSetting && (
-        <div className="fixed left-16 h-10 border rounded flex flex-row">
+        <div className="fixed left-16 h-10 border rounded flex flex-row bg-white">
           <div className="rounded h-10 p-2">
             <input id="default-range" type="range" defaultValue={pencilSize} min={1} max={20} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" onChange={(e) => {
               const { value } = e.target;
