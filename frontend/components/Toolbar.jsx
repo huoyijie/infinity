a410b8344e4834945f48409332c49d47a8c6f942
huoyijie <yijie.huo@foxmail.com>
2023-08-13 10:40:34 +0800

    fix onSetMode resetAll

--- frontend/components/Toolbar.jsx
+++ frontend/components/Toolbar.jsx
@@ -20,17 +20,22 @@ export default function ({ mode, setMode }) {
     setShowPencilSetting(false);
   };
 
+  const onSetMode = (mode) => {
+    setMode(mode);
+    resetAll();
+  };
+
   const WB = useContext(WBContext);
-  WB.onClick = resetAll;
+  WB.onClick = () => resetAll();
 
   return (
     <>
       {/* 画笔颜色选择 */}
       <div className="fixed z-50 top-8 left-5 flex flex-col gap-y-2">
         <SettingContext.Provider value={{ setShowColors, setShowOpacity, setShowPencilSetting, resetAll }}>
-          <Button Icon={PencilSquare} selected={mode === 'draw'} onClick={() => setMode('draw')} />
-          <Button Icon={Trash} selected={mode === 'eraser'} onClick={() => setMode('eraser')} />
-          <Button Icon={HandRaised} selected={mode === 'move'} onClick={() => setMode('move')} />
+          <Button Icon={PencilSquare} selected={mode === 'draw'} onClick={() => onSetMode('draw')} />
+          <Button Icon={Trash} selected={mode === 'eraser'} onClick={() => onSetMode('eraser')} />
+          <Button Icon={HandRaised} selected={mode === 'move'} onClick={() => onSetMode('move')} />
           <ColorPicker showColors={showColors} showOpacity={showOpacity} />
           <PencilSetting showPencilSetting={showPencilSetting} />
           <Button Icon={Question} />
