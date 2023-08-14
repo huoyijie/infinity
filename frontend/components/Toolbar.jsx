ea1412f884c6e1d646fe49cac1172f11f307d793
huoyijie <yijie.huo@foxmail.com>
2023-08-14 11:20:12 +0800

    refactor trash => correction tape

--- frontend/components/Toolbar.jsx
+++ frontend/components/Toolbar.jsx
@@ -1,6 +1,5 @@
 import Button from './Button';
 import PencilSquare from './assets/PencilSquare';
-import Trash from './assets/Trash';
 import HandRaised from './assets/HandRaised';
 import Question from './assets/Question';
 import ColorPicker from './ColorPicker';
@@ -9,6 +8,7 @@ import { useContext, useState } from 'react';
 import SettingContext from './SettingContext';
 import WBContext from './WBContext';
 import Undo from './assets/Undo';
+import Tape from './assets/Tape';
 
 export default function ({ mode, setMode }) {
   const [showColors, setShowColors] = useState(false);
@@ -37,7 +37,7 @@ export default function ({ mode, setMode }) {
       <div className="fixed z-50 top-8 left-5 flex flex-col gap-y-2">
         <SettingContext.Provider value={{ setShowColors, setShowOpacity, setShowPencilSetting, resetAll }}>
           <Button Icon={PencilSquare} selected={mode === 'draw'} onClick={() => onSetMode('draw')} />
-          <Button Icon={Trash} selected={mode === 'eraser'} onClick={() => onSetMode('eraser')} />
+          <Button Icon={Tape} selected={mode === 'eraser'} onClick={() => onSetMode('eraser')} />
           <Button Icon={HandRaised} selected={mode === 'move'} onClick={() => onSetMode('move')} />
           <Button Icon={Undo} disabled={!((mode === 'draw' || mode === 'eraser') && canUndo)} onClick={() => setTimeout(() => WB.undo(), 10)} />
           <ColorPicker showColors={showColors} showOpacity={showOpacity} />
--- frontend/components/assets/Undo.jsx
+++ frontend/components/assets/Undo.jsx
@@ -1,6 +1,6 @@
 export default function () {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
     </svg>
   );
